feaea59316bfb02ceb3950d81d3e2191cefae20b
Ahmad Khan <ahmsec@users.noreply.github.com>
2024-11-06 09:51:24 -0600

    support commonjs

--- README.md
+++ README.md
@@ -15,8 +15,19 @@ npm install otm-detector
 
 To use the OTM Detector module in your project, require it and call the `isOneTimeMail` function, passing the hostname as a parameter. The function will return `true` if the hostname belongs to a one-time mail service, and `false` otherwise.
 
+Import:
 ```javascript
 import { isOneTimeMail } from 'otm-detector';
+
+const hostname = 'example.com';
+const isOTM = await isOneTimeMail(hostname);
+
+console.log(`Is ${hostname} a one-time mail service? ${isOTM}`);
+```
+
+Require:
+```javascript
+const { isOneTimeMail } = require('otm-detector');
 
 const hostname = 'example.com';
 const isOTM = await isOneTimeMail(hostname);
@@ -31,6 +42,9 @@ We recommend using this module with existing burner email providers (for example
 ### Contributing
 
 Contributions are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request on the [GitHub repository](https://github.com/short-io/otm-detector).
+
+### Development
+After editing `index.js`, run `npx rollup index.js --file index.cjs --format cjs` to produce a CommonJS file as well.
 
 ### License
 
---
+++ index.cjs
@@ -0,0 +1,76 @@
+'use strict';
+
+var dns = require('node:dns/promises');
+
+const OTM_HOSTS = new Set([
+    "in.mail.tm",
+    "mail.onetimemail.org",
+    "mx.mail-data.net",
+    "mx1.emaildbox.pro",
+    "mx2.emaildbox.pro",
+    "mx3.emaildbox.pro",
+    "mx4.emaildbox.pro",
+    "mx5.emaildbox.pro",
+    "prd-smtp.10minutemail.com",
+    "emailfake.com",
+    "mx1.forwardemail.net",
+    "mx2.forwardemail.net",
+    "mx2.den.yt",
+    "route1.mx.cloudflare.net",
+    "route2.mx.cloudflare.net",
+    "route3.mx.cloudflare.net",
+    "mx1-hosting.jellyfish.systems",
+    "mx2-hosting.jellyfish.systems",
+    "mx3-hosting.jellyfish.systems",
+    "mx1.simplelogin.co",
+    "mx2.simplelogin.co",
+    "generator.email",
+]);
+const OTM_IPS = new Set([
+    "136.243.103.68",
+    "137.184.243.159",
+    "146.190.197.86",
+    "15.204.213.223",
+    "15.235.72.244",
+    "15.235.72.245",
+    "157.230.67.25",
+    "164.90.254.47",
+    "167.114.206.221",
+    "167.172.1.68",
+    "173.225.105.34",
+    "194.31.59.229",
+    "23.239.2.211",
+    "24.199.67.157",
+    "51.222.102.161",
+    "54.39.17.59",
+    "54.39.193.199",
+    "66.70.233.243", // openmail.pro
+    "92.255.84.131",
+    "96.126.99.62",
+]);
+const isOneTimeMail = async (domain, options = {}) => {
+    const otmDns = options.dns || dns;
+    try {
+        const records = await otmDns.resolveMx(domain);
+        if (records.length === 0) { // this email is invalid, but we are not a validator
+            return false
+        }
+        if (records.some((record) => OTM_HOSTS.has(record.exchange))) {
+            return true
+        }
+        // check first record for new
+        const mxHost = records[0].exchange;
+        const mxAddresses = await otmDns.resolve4(mxHost);
+        if (mxAddresses.some((address) => OTM_IPS.has(address))) {
+            return true
+        }
+        return false;
+    } catch (e) {
+        if (e.code === "ENOTFOUND") {
+            return false
+        }
+        throw e
+    }
+};
+
+exports.isOneTimeMail = isOneTimeMail;
